73347e6d6e115d84b65a54a40437a8caabc4f7eb
roissyahf <roissyahfk@gmail.com>
2023-11-13 23:25:21 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+import pickle
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+# load the model and tokenizer
+model = tf.keras.models.load_model('model.h5')
+tokenizer = pickle.load(open('tokenizer.pickle', 'rb'))
+
+# process input news
+def clean_input(text):
+    #remove html
+    html = re.compile(r"<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});")
+    text = re.sub(html, "", text)
+
+    #remove_non_ascii characters
+    text = re.sub(r'[^\x00-\x7f]',r'', text)
+
+    # remove_special_characters
+    emoji_pattern = re.compile(
+        '['
+        u'\U0001F600-\U0001F64F'  # emoticons
+        u'\U0001F300-\U0001F5FF'  # symbols & pictographs
+        u'\U0001F680-\U0001F6FF'  # transport & map symbols
+        u'\U0001F1E0-\U0001F1FF'  # flags (iOS)
+        u'\U00002702-\U000027B0'
+        u'\U000024C2-\U0001F251'
+        ']+',
+        flags=re.UNICODE)
+    text = emoji_pattern.sub(r'', text)
+
+    # remove_punct
+    text = re.sub(r'[]!"$%&\'()*+,./:;=#@?[\\^_`{|}~-]+', "", text)
+    
+    # lowercase text
+    text = text.lower()
+
+    return text
+
+# prediction route
+@app.route("/predict", methods=["POST"])
+def predict(input_news):
+    # clean the input_news
+    text = clean_input(input_news)
+    # tokenize the news
+    text_sequences = tokenizer.texts_to_sequences(text)
+    text_padded = pad_sequences(text_sequences, maxlen=1313, padding='post', truncating='post')
+    # prediction
+    tresh = 0.55
+    proba = model.predict(text_padded)
+    class_id = (proba > tresh).astype(int)
+
+    if class_id == 0:
+        result_string = f'HOAX dengan tingkat kepercayaan {proba}'
+    else:
+        result_string = f'VALID dengan tingkat kepercayaan {proba}'
+    
+    return result_string
+
+if __name__ == '__main__':
+    # use port default 8080
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
